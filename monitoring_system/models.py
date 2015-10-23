a7231f656538ae846bab07aecac092cfd2258431
njthykkathu <nijin.thykkathu@stfc.ac.uk>
2015-10-23 15:48:42 +0100

    Made changes to the data model

--- monitoring_system/models.py
+++ monitoring_system/models.py
@@ -4,22 +4,34 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-class CommonData(models.Model):
+class Resource(models.Model):
+    CPU = 0
+    RAM = 1
+    __MetricType =(
+        (CPU, 'CPU Cores'),
+        (RAM, 'RAM Used'),
+    )
+
     Org_ID = models.CharField(max_length=50)
     Vm_ID = models.CharField(max_length=50)
     Value = models.CharField(max_length=5)
     Date_Time = models.DateTimeField(auto_now_add=True)
+    Metric_Type = models.CharField(max_length=3, choices=__MetricType)
 
     def __str__(self):
          return self.Org_ID
 
-    class Meta:
-        abstract = True
+    # class Meta:
+    #     abstract = True
 
 
-class Resource(CommonData):
-    metric_list = (
-        ('CPU', 'CPU Cores'),
-        #('RAM', 'RAM'),
-    )
-    Metric_Type = models.CharField(max_length=3, choices=metric_list)
+# class Resource(CommonData):
+#     metric_list = (
+#         ('CPU', 'CPU Cores'),
+#         #('RAM', 'RAM'),
+#     )
+#     Metric_Type = models.CharField(max_length=3, choices=((RAM, 'RAM Used'), (CPU, 'CPU Cores')))
+
+
+
+#Resource.objects.filter(metric_type = Resource.CPU)
