73359ab9aecf0119e91d0ad91b6d751fd0fb71bd
njthykkathu <nijin.thykkathu@stfc.ac.uk>
2015-11-03 17:13:41 +0000

    Made changes to start displaying data to django

--- monitoring_system/views.py
+++ monitoring_system/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+from .models import Resource
+
+
 # Create your views here.
+
+
+def index(request):
+    table = Resource.objects.all()
+    return HttpResponse(table)
+
