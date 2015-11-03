c782bc6ace44ecc6f883da330be5e61e15ccb3b4
njthykkathu <nijin.thykkathu@stfc.ac.uk>
2015-11-03 17:14:10 +0000

    Added url

--- jasmin_monitoring/urls.py
+++ jasmin_monitoring/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^monitoring/', include('monitoring_system.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
