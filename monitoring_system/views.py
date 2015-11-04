8dd35d9ed25e47a524868b113384a1de13d92e6a
njthykkathu <nijin.thykkathu@stfc.ac.uk>
2015-11-04 17:01:39 +0000

    Added code to display the data from the database on django

--- monitoring_system/views.py
+++ monitoring_system/views.py
@@ -3,12 +3,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Resource
+from django.template import RequestContext, loader
+from django.shortcuts import render_to_response
+
+
 
 
 # Create your views here.
 
 
-def index(request):
-    table = Resource.objects.all()
-    return HttpResponse(table)
+def resource(request):
+    total_query = Resource.objects.all()
+    cpu_query = Resource.objects.filter(Metric_Type=Resource.CPU)
+    ram_query = Resource.objects.filter(Metric_Type=Resource.RAM)
+    template = loader.get_template('monitoring_system/main.html')
+    context = RequestContext(request, {'total_data': total_query, 'cpu': cpu_query, 'ram': ram_query, })
+    return HttpResponse(template.render(context))
 
+
+# Alternative method
+
+#def index(request):
+ # return render_to_response('monitoring_system/main.html', {'obj': Resource.objects.all(), 'cpu': Resource.objects.filter(Metric_Type=Resource.CPU)})
